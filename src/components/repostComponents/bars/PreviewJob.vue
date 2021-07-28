79807d2d66a628bde5f3c335d8070c8589dfb61d
Anas <anas@kasmi.dev>
2021-07-28 18:34:46 +0100

    renamed steps

--- src/components/repostComponents/bars/PreviewJob.vue
+++ src/components/repostComponents/bars/PreviewJob.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar dark color="#0f3057">
-    <v-toolbar-title class="ml-10">8- Preview Job page</v-toolbar-title>
+    <v-toolbar-title class="ml-10">7- Preview Job page</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
