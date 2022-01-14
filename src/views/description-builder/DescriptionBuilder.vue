8c414d82892d792e52e23025b24b643c60727004
anas kasmi <anaskasmi98@gmail.com>
2022-01-14 15:20:37 +0100

    Drawer done

--- src/views/description-builder/DescriptionBuilder.vue
+++ src/views/description-builder/DescriptionBuilder.vue
@@ -1,17 +1,48 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list rounded >
-        <v-list-item-group v-model="selectedItem" color="primary">
-          <v-list-item v-for="(item, i) in links" :key="i">
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
+    <v-navigation-drawer color="#f6f9fb" v-model="drawer" app>
+      <v-list>
+        <!-- home page -->
+        <v-list-item>
+          <v-img
+            height="100"
+            width="120"
+            contain
+            class="grow"
+            style="cursor: pointer"
+            :src="require('@/assets/img/logo.png')"
+          >
+          </v-img>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-group
+          v-for="(item, i) in links"
+          :key="i"
+          :prepend-icon="item.icon"
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </template>
+
+          <v-list-item class="ml-10" link>
+            <v-icon small class="mr-2 info--text text--darken-3"
+              >mdi-checkbox-outline</v-icon
+            >
+            <v-list-item-title class="info--text text--darken-3"
+              >Items</v-list-item-title
+            >
           </v-list-item>
-        </v-list-item-group>
+
+          <v-list-item class="ml-10" link>
+            <v-icon small class="mr-2 info--text text--darken-3"
+              >mdi-checkbox-multiple-marked-outline
+            </v-icon>
+            <v-list-item-title class="info--text text--darken-3"
+              >Sets Of Items</v-list-item-title
+            >
+          </v-list-item>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
 
@@ -19,6 +50,10 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Job Description Builder</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="pink lighten-1" tile elevation="1">
+        <v-icon class="mr-2">mdi-pencil</v-icon> Job Builder</v-btn
+      >
     </v-app-bar>
 
     <v-main> </v-main>
@@ -32,31 +67,31 @@ export default {
       selectedItem: null,
       links: [
         {
-          icon: "mdi-domain",
+          icon: "mdi-account-hard-hat ",
           text: "positions",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-archive-check ",
           text: "Apply Methods",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-format-list-checks ",
           text: "Qualities",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-medical-bag ",
           text: "Compensation",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-file-compare  ",
           text: "Experience",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-puzzle-star ",
           text: "Incentive",
         },
         {
-          icon: "mdi-pencil",
+          icon: "mdi-tag-multiple ",
           text: "Tags",
         },
       ],
